c52c9e4b114c5e3a04d4adf99f56915bdfba3b54
mizlin <you@example.com>
2015-09-01 18:36:41 +0200

    variable name changed

--- gameboard.java
+++ gameboard.java
@@ -15,19 +15,20 @@ public class gameboard {
     int sizeOfSpace = 2;
     int numOfLettersInLine = sizeOfBoardSide * 2 + sizeOfBoardSide - 1;
     String space = "  ";
-    String separator = "|";
+    String verLine = "|";
+    String  horLine = "-";
 
     for (int i = 1; i <= sizeOfBoardSide + 2; i++) {
       if (i % 2 == 0) {
         for (int n = 0; n < numOfLettersInLine; n++) {
-          System.out.print("-");
+          System.out.print(horLine);
         }
       } else {
           for (int n = 0; n < sizeOfBoardSide - 1; n += sizeOfSpace) {
             for (int k = 0; k <= sizeOfSpace; k++) {
               System.out.print(space);
               if (k != sizeOfSpace) {
-                System.out.print(separator);
+                System.out.print(verLine);
               }
             }
           }
