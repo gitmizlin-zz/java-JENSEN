5e09ddcd570eff71644ffdfb5b93e59e24ad2f5b
mizlin <mizlin@gmail.com>
2015-10-07 23:01:49 +0200

    r

--- TestStatic.java
+++ TestStatic.java
@@ -3,13 +3,23 @@
 public class TestStatic {
 	static int totalAmount = 0;
 	int id;
+	int age;
+	String name;
 
 	TestStatic() { // constructor. Called whenever you create a new instance of the class
 		totalAmount++;
 		id = totalAmount;
 	}
 
-	static int getTotalAmount() { // why need this method? you can just call the variable totalAmount?
+	TestStatic(int age) { 
+		this.age = age;
+	}
+
+	TestStatic(String name) {
+		this.name = name;
+	}
+
+	static int getTotalAmount() { // why need this method? you can just call the variable totalAmount? -> easier to fix later than fixing the variable
 		return totalAmount;
 	}
 
@@ -22,9 +32,15 @@ public class TestStatic {
 		TestStatic obj2 = new TestStatic();
 		TestStatic obj3 = new TestStatic();
 		TestStatic obj4 = new TestStatic();
-		TestStatic obj5 = new TestStatic();
-		TestStatic obj6 = new TestStatic();
+		TestStatic obj5 = new TestStatic("hana");
+		TestStatic obj6 = new TestStatic(11);
 
         System.out.println("Number of objects created: " + totalAmount);
+        System.out.println(obj5);
+        System.out.println(obj6.age);
+	}
+
+	public String toString() { 
+		return name;
 	}
 }
