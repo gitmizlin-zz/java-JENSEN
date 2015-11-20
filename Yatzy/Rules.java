1f66e59850a0d6b810b1413fa3510408b7f040d7
mizlin <mizlin@gmail.com>
2015-11-20 16:04:46 +0100

    rules

--- Yatzy/Rules.java
+++ Yatzy/Rules.java
@@ -6,14 +6,20 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Rules extends JFrame {
     static JPanel rulePanel;
     JButton ruleButton;
     JTextArea textArea;
+    static JFrame frame = new JFrame("Yatzy rules");
+    static JPanel panel;
 
     Rules() {
+        panel = new JPanel();
         rulePanel = new JPanel();
         rulePanel.setBackground(Color.BLUE);
         add(rulePanel);
@@ -23,70 +29,63 @@ public class Rules extends JFrame {
         rulePanel.add(ruleButton);
     }
 
-    // inte JOptionpane, lite text
 // Använd en annan panel som öppnas separat.
 
     class RulesInnerListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
+            readFile();
             createFrame();
-            ArrayList<String> records = new ArrayList<>();
+
+        }
+
+    public void createFrame() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                JTextArea textArea = new JTextArea(30, 50);
+                textArea.append(readFile());
+                textArea.setEditable(false);
+                panel.add(textArea);
+
+                JScrollPane scroller = new JScrollPane(textArea);
+                scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+                scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+                panel.add(scroller);
+                frame.getContentPane().add(BorderLayout.CENTER, panel);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                frame.setResizable(true);
+            }
+        });
+    }
+        public String readFile() {
+            String line;
+            String txt = "";
             String filename = "yatzyrules.txt";
+//
+            ArrayList<String> records = new ArrayList<>();
 
             try {
-                BufferedReader reader = new BufferedReader(new FileReader(filename));
-                String line;
+                BufferedReader reader = new BufferedReader(new FileReader((filename)));
+
                 while ((line = reader.readLine()) != null) {
+                    txt = txt + "\n" + line;
                     records.add(line);
                 }
-                String listString = "";
-
-                for (String s : records) {
-                    listString += s + "\t";
-                }
-                rulePanel.add(textArea);
                 reader.close();
 
             } catch (Exception e) {
                 System.err.format("Exception occurred trying to read '%s'.", filename);
                 e.printStackTrace();
             }
+            return txt;
         }
-    }
-
-    public static void createFrame() {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new JFrame("Yatzy rules");
-                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                try
-                {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                JPanel panel = new JPanel();
-                panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-                panel.setOpaque(true);
-                JTextArea textArea = new JTextArea(30, 50);
-                textArea.setWrapStyleWord(true);
-                textArea.setEditable(false);
-                textArea.setFont(Font.getFont(Font.SANS_SERIF));
-                JScrollPane scroller = new JScrollPane(textArea);
-                scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-                scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-                DefaultCaret caret = (DefaultCaret) textArea.getCaret();
-                caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-                panel.add(scroller);
-                frame.getContentPane().add(BorderLayout.CENTER, panel);
-                frame.pack();
-
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                frame.setResizable(false);
-
-            }
-        });
     }
 }
